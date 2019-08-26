189541f0ea195ea9d59259f38e0fb6a8f7adf5cb
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-26 12:59:49 -0700

    constants

---
+++ consts.go
@@ -0,0 +1,9 @@
+package utf8n
+
+const (
+	LF  = '\n'     // line feed
+	CR  = '\r'     // carriage return
+	NEL = '\u0085' // next line
+	LS  = '\u2028' // line separator
+	PS  = '\u2029' // paragraph separator
+)
