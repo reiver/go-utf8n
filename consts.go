14fcc27a87389e817d5c1ad0fe15f8a874b3f6f4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-09-05 09:51:47 -0700

    utf8n.UTF8Max

--- consts.go
+++ consts.go
@@ -7,3 +7,7 @@ const (
 	LS  = '\u2028' // line separator
 	PS  = '\u2029' // paragraph separator
 )
+
+const (
+	UTF8Max = 4   // the maximum number of bytes a UTF-8 encoded Unicode character can be.
+)
